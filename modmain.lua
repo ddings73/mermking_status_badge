9a8def84132210508a44538f60e6977ea2a01660
ddings73 <ddings7303@gmail.com>
2024-12-29 18:05:17 +0900

    0.1.0ver 주석 추가

--- modmain.lua
+++ modmain.lua
@@ -3,26 +3,28 @@ local MermKingHealthBadge = require "widgets/mermking_health_badge"
 
 local hungerbadge = nil
 local healthbadge = nil
-local king = nil
 local player_inst = nil
-local MERM_ONLY = GetModConfigData("MERM_ONLY")
+local MERM_ONLY = GetModConfigData("MERM_ONLY")         -- modinfo.lua의 옵션 값
 
+-- 인게임 아이콘 표시를 위한 애니메이션 파일 추가
 Assets = {
     Asset("ANIM", "anim/mermking_hunger_meter.zip"),
     Asset("ANIM", "anim/mermking_health_meter.zip")
 }
 
+-- 확인이 필요한 모드 목록
 local CHECK_MODS = {
     ["workshop-376333686"] = "COMBINED_STATUS",
     ["CombinedStatus"] = "COMBINED_STATUS",
 }
 
+-- 플레이어가 활성화한 모드 리스트 확인
 local HAS_MOD = {}
---If the mod is already loaded at this point
+-- 로드된 모드 목록 확인
 for mod_name, key in pairs(CHECK_MODS) do
     HAS_MOD[key] = HAS_MOD[key] or (GLOBAL.KnownModIndex:IsModEnabled(mod_name) and mod_name)
 end
---If the mod hasn't loaded yet
+-- 로드되지 않은 모드 목록 확인
 for k, v in pairs(GLOBAL.KnownModIndex:GetModsToLoad()) do
     local mod_type = CHECK_MODS[v]
     if mod_type then
@@ -30,28 +32,36 @@ for k, v in pairs(GLOBAL.KnownModIndex:GetModsToLoad()) do
     end
 end
 
+-- 어인왕 허기 상승 효과
 local function hungerIncrease()
     hungerbadge:PulseGreen()
     GLOBAL.TheFrontEnd:GetSound():PlaySound("dontstarve/HUD/hunger_up")
 end
 
+-- 어인왕 체력 감소 효과
 local function healthDecrease()
     healthbadge:PulseRed()
     GLOBAL.TheFrontEnd:GetSound():PlaySound("dontstarve/HUD/helath_down") 
 end
 
+-- 플레이어 HUD 상태 표현 클래스 확장 
+-- self는 출력 시, Status 객체로 표현됨
 AddClassPostConstruct("widgets/statusdisplays", function(self)
     if not self.owner then return end
     
+    -- 뱃지 객체 생성 및 위치설정
 	self.hungerbadge = self:AddChild(MermKingHungerBadge(self.owner, HAS_MOD.COMBINED_STATUS))
 	self.hungerbadge:SetPosition(-120,20)
     
     self.healthbadge = self:AddChild(MermKingHealthBadge(self.owner, HAS_MOD.COMBINED_STATUS))
     self.healthbadge:SetPosition(-200,20)
 
+    -- 외부에서 badge 객체에 접근하기 위해 상위 scope에 저장
+    -- 설정하지 않으면, 서버와 클라이언트 scope로 인해 외부에서 접근 불가 
     hungerbadge = self.hungerbadge;
     healthbadge = self.healthbadge;
 
+    -- 내부에서 뱃지표현에 사용할 변수 생성
     self.last_hunger = 0
     self.current_hunger = 0
     self.max_hunger = TUNING.MERM_KING_HUNGER
@@ -59,30 +69,37 @@ AddClassPostConstruct("widgets/statusdisplays", function(self)
     self.last_health = 0
     self.current_health = 0
     
+    -- 매 프레임마다 반복하여 작업을 수행하는 entity 생성
     local entity = GLOBAL.CreateEntity()
     entity:DoPeriodicTask(0, function()
         if GLOBAL.ThePlayer ~= nil then 
+
+            -- 현재 프레임의 상태 값 반영
             self.current_hunger = GLOBAL.ThePlayer.player_classified.mermking_hunger_current
             self.max_hunger = GLOBAL.ThePlayer.player_classified.mermking_hunger_max
             self.current_health = GLOBAL.ThePlayer.player_classified.mermking_health_current
             
+            -- 뱃지 표현을 위한 조건 검증
             if self.current_health > 0 and (not MERM_ONLY or GLOBAL.ThePlayer.prefab == "wurt" or GLOBAL.ThePlayer.player_classified.equipped_hat == "mermhat") then
                 self.healthbadge:Show()
                 self.hungerbadge:Show()
 
+                -- 뱃지 수치 갱신 및 효과 출력
                 self.hungerbadge:SetPercent(self.current_hunger, self.max_hunger)
                 if self.last_hunger ~= nil and self.current_hunger ~= nil and self.current_hunger > self.last_hunger then
                     hungerIncrease()
                 end
 
-
                 self.healthbadge:SetPercent(self.current_health, TUNING.MERM_KING_HEALTH)
                 if self.last_health ~= nil and self.current_health ~= nil and self.current_health < self.last_health then
                     healthDecrease()
                 end
-
+                
+                -- combined status 모드에 따른 UI 수정
                 if HAS_MOD.COMBINED_STATUS then
                     local Text = require("widgets/text")
+
+                    -- 뱃지 위치 및 크기 조정
                     self.hungerbadge:SetPosition(-124, 35)
                     self.hungerbadge.rate = self.hungerbadge:AddChild(Text(GLOBAL.NUMBERFONT, 28))
                     self.hungerbadge.rate:SetPosition(2, -40.5, 0)
@@ -94,76 +111,9 @@ AddClassPostConstruct("widgets/statusdisplays", function(self)
                     self.healthbadge.rate:SetPosition(2, -40.5, 0)
                     self.healthbadge.rate:SetScale(1,.78,1)
                     self.healthbadge.rate:Hide()
-
-                    self.inst:DoTaskInTime(0, function(inst)
-                        local _ShowStatusNumbers = self.ShowStatusNumbers
-                        function self:ShowStatusNumbers()
-                            _ShowStatusNumbers(self)
-                            if self.hungerbadge ~= nil then
-                                self.hungerbadge.rate:Show()
-                                self.hungerbadge.num:Hide()
-                            end
-
-                            if self.healthbadge ~= nil then
-                                self.healthbadge.rate:Show()
-                                self.healthbadge.num:Hide()
-                            end
-                        end
-
-                        local _HideStatusNumbers = self.HideStatusNumbers
-                        function self:HideStatusNumbers()
-                            _HideStatusNumbers(self)
-                            if self.hungerbadge ~= nil then
-                                self.hungerbadge.rate:Hide()
-                                self.hungerbadge.num:Show()
-                            end
-
-                            if self.healthbadge ~= nil then
-                                self.healthbadge.rate:Hide()
-                                self.healthbadge.num:Show()
-                            end
-                        end
-                    end)
-
-                    -- -- 마우스 벗어남
-                    -- local _OnLoseFocus = self.hungerbadge.OnLoseFocus
-                    -- function self.hungerbadge:OnLoseFocus()
-                    --     _OnLoseFocus(self)
-                    --     self.rate:Hide()
-                    -- end
-                    
-                    -- -- 마우스 들어옴
-                    -- local _OnGainFocus = self.hungerbadge.OnGainFocus
-                    -- function self.hungerbadge:OnGainFocus()
-                    --     _OnGainFocus(self)
-                    --     if self.active then
-                    --         self.rate:Show()
-                    --     end
-                    -- end
-                    
-                    -- 배 위에 있을 때 뱃지 위치관련
-                    -- function self.hungerbadge:OnHide()
-                    --     if self.parent ~= nil and self.parent.boatmeter then
-                    --         self.parent.boatmeter:SetPosition(-62, -52)
-                    --     end
-                    -- end
-
-                    -- function self.hungerbadge:OnShow()
-                    --     if self.parent ~= nil and self.parent.boatmeter then
-                    --         self.parent.boatmeter:SetPosition(-62, -139)
-                    --     end
-                    -- end
-
-                    -- if self.boatmeter then
-                    --     self.boatmeter.inst:ListenForEvent("open_meter", function(inst)
-                    --         if inst.widget.parent.hungerbadge.shown then
-                    --             inst.widget:SetPosition(-62, -139)
-                    --         else
-                    --             inst.widget:SetPosition(-62, -52)
-                    --         end
-                    --     end)
-                    -- end
                 else
+        
+                    -- 뱃지 내 수치표현에 사용되는 텍스트 설정 수정
                     self.hungerbadge.num:SetSize(25)
                     self.hungerbadge.num:SetScale(1,.9,1)
                     self.hungerbadge.num:SetPosition(3, 3)
@@ -171,34 +121,9 @@ AddClassPostConstruct("widgets/statusdisplays", function(self)
                     self.healthbadge.num:SetSize(25)
                     self.healthbadge.num:SetScale(1,.9,1)
                     self.healthbadge.num:SetPosition(3, 3)
-
-
-                    local _ShowStatusNumbers = self.ShowStatusNumbers
-                    function self:ShowStatusNumbers()
-                        _ShowStatusNumbers(self)
-                        if self.hungerbadge ~= nil then
-                            self.hungerbadge.num:Show()
-                        end
-
-                        if self.healthbadge ~= nil then
-                            self.healthbadge.num:Show()
-                        end
-                    end
-
-                    local _HideStatusNumbers = self.HideStatusNumbers
-                    function self:HideStatusNumbers()
-                        _HideStatusNumbers(self)
-                        if self.hungerbadge ~= nil then
-                            self.hungerbadge.num:Hide()
-                        end
-
-                        
-                        if self.healthbadge ~= nil then
-                            self.healthbadge.num:Hide()
-                        end
-                    end
                 end
                 
+                -- 다음 task에서 수치 증감을 확인하기 위해 현재 값 저장
                 self.last_hunger = self.current_hunger
                 self.last_health = self.current_health
             else 
@@ -209,13 +134,16 @@ AddClassPostConstruct("widgets/statusdisplays", function(self)
     end)
 end)
 
+-- player_classified prefab 확장
 AddPrefabPostInit("player_classified", function(inst)
+    -- shard간 RPC통신 시점에서 접근하기 위해 외부 scope에 inst 저장
     player_inst = inst
+
     
-    if VISIBLEOPTION then
+    -- 어인왕의 현재 허기수치 관리를 위한 변수 및 네트워크 변수 추가
+    if MERM_ONLY then
         inst.equipped_hat = ""
         inst.net_equipped_hat = GLOBAL.net_string(inst.GUID, "equipped_hat", "equipped_hat_dirty")
-
         inst:ListenForEvent("equipped_hat_dirty", function(inst)
             inst.equipped_hat = inst.net_equipped_hat:value()
         end)
@@ -223,37 +151,39 @@ AddPrefabPostInit("player_classified", function(inst)
 
     inst.mermking_hunger_current = 0
     inst.net_mermking_hunger_current = GLOBAL.net_int(inst.GUID, "mermking_hunger_current", "mermking_hunger_current_dirty")
-    
     inst:ListenForEvent("mermking_hunger_current_dirty", function(inst)
         inst.mermking_hunger_current = inst.net_mermking_hunger_current:value()
     end)
 
     inst.mermking_hunger_max = TUNING.MERM_KING_HUNGER
     inst.net_mermking_hunger_max = GLOBAL.net_int(inst.GUID, "mermking_hunger_max", "mermking_hunger_max_dirty")
-
     inst:ListenForEvent("mermking_hunger_max_dirty", function(inst)
         inst.mermking_hunger_max = inst.net_mermking_hunger_max:value()
     end)
     
     inst.mermking_health_current = 0
     inst.net_mermking_health_current = GLOBAL.net_int(inst.GUID, "mermking_health_current", "mermking_health_current_dirty")
-    
     inst:ListenForEvent("mermking_health_current_dirty", function(inst)
         inst.mermking_health_current = inst.net_mermking_health_current:value()
     end)
 
+    -- 프레임마다 mermkingmanager의 상태값으로 네트워크 변수를 갱신
     inst:DoPeriodicTask(0, function()
         local mermkingmanager = GLOBAL.TheWorld.components.mermkingmanager
         if mermkingmanager ~= nil then
+
+            -- 현재 플레이어 Shard와 어인왕의 Shard가 동일한 위치인지 확인하여 분기
             if mermkingmanager:HasKingLocal() then
-                king = mermkingmanager:GetKing()
+                local king = mermkingmanager:GetKing()
                 inst.net_mermking_hunger_current:set(king.components.hunger.current) 
                 inst.net_mermking_hunger_max:set(king.components.hunger.max)
                 inst.net_mermking_health_current:set(king.components.health.currenthealth)
             elseif mermkingmanager:HasKingAnywhere() then
-                -- 반대쪽(동굴 or 지상)으로 시그널 전달
+                -- 반대쪽 Shard(동굴 or 지상)로 시그널 전달
+                -- 두번째 인자가 nil인 경우, 연결된 모든 Shard로 요청을 전송
                 SendModRPCToShard(GetShardModRPC(modname, "mermking_update"), nil, nil, nil, nil) 
             else
+                -- 어인왕이 존재하지 않을 경우, 기본값으로 초기화
                 inst.net_mermking_hunger_current:set(0) 
                 inst.net_mermking_hunger_max:set(TUNING.MERM_KING_HUNGER)
                 inst.net_mermking_health_current:set(0)
@@ -262,8 +192,31 @@ AddPrefabPostInit("player_classified", function(inst)
     end)
 end)
 
+-- shard(동굴, 지상) 간 데이터 교환을 위한 원격 프로시저 핸들러
+-- RPC 시그널을 생성한 Shard와 동일한지 확인하고, 상태값의 유무에 따라 수행할 동작 분기
+-- shardId는 RPC 요청을 생성한 Shard를 의미
+AddShardModRPCHandler(modname, "mermking_update", function(shardId, hunger_current, hunger_max, health_current)
+    if GLOBAL.TheShard:GetShardId() ~= tostring(shardId) then
+        if hunger_current ~= nil and hunger_max ~= nil and health_current ~= nil then
+            player_inst.net_mermking_hunger_current:set(hunger_current)
+            player_inst.net_mermking_hunger_max:set(hunger_max)
+            player_inst.net_mermking_health_current:set(health_current)
+        else
+            local king = GLOBAL.TheWorld.components.mermkingmanager:GetKing()
+            GLOBAL.TheWorld:DoTaskInTime(0, function()
+                SendModRPCToShard(GetShardModRPC(modname, "mermking_update"), shardId, 
+                    king.components.hunger.current, 
+                    king.components.hunger.max, 
+                    king.components.health.currenthealth)
+            end) 
+        end
+    end
+end)
+
+-- 플레이어의 모자슬롯 아이템 확인을 위해 플레이어가 생성된 후에 수행할 작업 확장
 AddPlayerPostInit(function(inst)
-    if VISIBLEOPTION then
+    if MERM_ONLY then
+        -- 플레이어가 서버에 접속했을 경우
         GLOBAL.TheWorld:ListenForEvent("ms_playerjoined", function(self)
             local hat = inst.components.inventory:GetEquippedItem(GLOBAL.EQUIPSLOTS.HEAD)
             if hat ~= nil then 
@@ -271,6 +224,7 @@ AddPlayerPostInit(function(inst)
             end
         end)
 
+        -- 플레이어가 장비를 착용했을 경우
         inst:ListenForEvent("equip", function(owner, data)
             if data.item ~= nil then
                 local equipslot = data.item.replica.equippable:EquipSlot()
@@ -280,6 +234,7 @@ AddPlayerPostInit(function(inst)
             end
         end)
 
+        -- 플레이어가 장비를 해제했을 경우
         inst:ListenForEvent("unequip", function(owner, data)
             if data.item ~= nil then
                 local equipslot = data.item.replica.equippable:EquipSlot()
@@ -288,22 +243,5 @@ AddPlayerPostInit(function(inst)
                 end
             end
         end)
-    end
-end)
-
-AddShardModRPCHandler(modname, "mermking_update", function(shardId, hunger_current, hunger_max, health_current)
-    if GLOBAL.TheShard:GetShardId() ~= tostring(shardId) then
-        if hunger_current ~= nil and hunger_max ~= nil and health_current ~= nil then
-            -- if player_inst.mermking_hunger_current < hunger_current then
-                player_inst.net_mermking_hunger_current:set(hunger_current)
-                player_inst.net_mermking_hunger_max:set(hunger_max)
-                player_inst.net_mermking_health_current:set(health_current)
-            -- end
-        else
-            local king = GLOBAL.TheWorld.components.mermkingmanager:GetKing()
-            GLOBAL.TheWorld:DoTaskInTime(0, function()
-                SendModRPCToShard(GetShardModRPC(modname, "mermking_update"), shardId, king.components.hunger.current, king.components.hunger.max, king.components.health.currenthealth)
-            end) 
-        end
     end
 end)
--- scripts/widgets/mermking_hunger_badge.lua
+++ scripts/widgets/mermking_hunger_badge.lua
@@ -1,33 +1,35 @@
 local Badge = require "widgets/badge"
 local UIAnim = require "widgets/uianim"
 
+-- 뱃지에 표시될 아이콘 애니메이션
 Assets = {
     Asset("ANIM", "anim/mermking_hunger_meter.zip"),
 }
 
 local MermKingBadge = Class(Badge, function(self, owner, combined_status)
 	self.owner = owner
-	self.comined_status = combined_status or false
-	self.val = 0
-	self.max = 0
+	self.comined_status = combined_status or false 	-- combined_status 모드 적용체크
+	self.val = 0									-- 현재 수치
+	self.max = 0									-- 최대 수치
 
-	Badge._ctor(self, nil, owner, {0, 0.5, 0, 1}, "mermking_hunger_meter", nil, nil, true)
+	-- Badge 초기화 (애니메이션 설정 및 뱃지 객체 생성)
+	Badge._ctor(self, nil, owner, {0, 0.5, 0, 1}, "mermking_hunger_meter", nil, nil, true) 
 
-	self.sanityarrow = self.underNumber:AddChild(UIAnim())
-	self.sanityarrow:GetAnimState():SetBank("sanity_arrow")
-	self.sanityarrow:GetAnimState():SetBuild("sanity_arrow")
-	self.sanityarrow:GetAnimState():PlayAnimation("neutral")
-	self.sanityarrow:GetAnimState():AnimateWhilePaused(false)
-	self.sanityarrow:SetClickable(false)
+	self.sanityarrow = self.underNumber:AddChild(UIAnim())		-- 뱃지 내부의 수치증감으로 인한 화살표 표현
+	self.sanityarrow:GetAnimState():SetBank("sanity_arrow")		-- 화살표 애니메이션의 표현을 위한 데이터 그룹
+	self.sanityarrow:GetAnimState():SetBuild("sanity_arrow")	-- 화살표 애니메이션의 이미지 설정
+	self.sanityarrow:GetAnimState():PlayAnimation("neutral")	-- 화살표 애니메이션의 타입(방향) 설정
+	self.sanityarrow:GetAnimState():AnimateWhilePaused(false)	-- 일시정지 상태에서 애니메이션의 출력여부
+	self.sanityarrow:SetClickable(false)						-- 클릭 가능여부
 end)
 
 function MermKingBadge:SetPercent(val, max)
 	if val == nil then return end
-	self.val = val
-	self.max = max
+	self.val = val									-- 현재 수치 갱신
+	self.max = max									-- 최대 수치 갱신
 
-	Badge.SetPercent(self, val / max, max)
-	self.num:SetString(string.format("%d", val))
+	Badge.SetPercent(self, val / max, max)			-- 뱃지에 게이지 관리를 위한 퍼센트 갱신
+	self.num:SetString(string.format("%d", val))	-- 뱃지에 표기될 숫자(현재 수치) 갱신
 end
 
 return MermKingBadge
