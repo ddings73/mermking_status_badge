751127f41a14e968ccd59ca2dc888aba8d7466a9
ddings73 <ddings7303@gmail.com>
2024-12-29 15:15:33 +0900

    modinfo 주석 추가

--- modinfo.lua
+++ modinfo.lua
@@ -4,24 +4,28 @@ author = "ddings"
 version = "0.1.0"
 forumthread = ""
 
-icon_atlas = "modicon.xml"
-icon = "modicon.tex"
+-- 인게임에서 보여지는 모드 아이콘 정보
+icon_atlas = "modicon.xml" -- tex파일 내의 이미지에 대한 영역배치 정보
+icon = "modicon.tex" -- 아이콘 파일
 
-all_clients_require_mod = true
-client_only_mod = false
+all_clients_require_mod = true -- 서버 모드
+client_only_mod = false -- 클라이언트 모드
 
-dont_starve_compatible = false
-reign_of_giants_compatible = false
-shipwrecked_compatible = false
-dst_compatible = true
+-- 호환여부 체크
+dont_starve_compatible = false -- 굶지마 싱글
+reign_of_giants_compatible = false  -- 거인의 군림 dlc
+shipwrecked_compatible = false -- 난파선 dlc
+dst_compatible = true -- 굶지마 투게더
 
-api_version = 10
+-- API 버전 
+-- scripts의 mods.lua 파일에서 확인할 수 있으며, 굶지마 투게더의 경우 10
+api_version = 10 
 
-
+-- 모드 옵션 설정정보
 configuration_options =
 {
     {
-        name = "VISIBLEOPTION",
+        name = "MERM_ONLY",
         label = "Visible Player",
         hover = "어인왕의 체력 및 허기수치를 보여줄 캐릭터 범위를 설정합니다.",
         options =   {
